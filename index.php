79fbf0cc9eb4d64b73dc3da25c61b4b05016261e
nikonik11 <rouanetnicolas@orange.fr>
2020-09-08 16:53:05 +0200

    reload

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
-<?php
-
-?>
-
-
-
-
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8"/>
-    <title>Yuka</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <title>Connexion</title>
 </head>
+<body>
+    <h1> Connexion </h1>
+    <form method="post" action="connexion.php">
+        <table>
+            <tr>
+                <td>Pseudo</td>
+                <td> <input type="text" name="pseudo" placeholder="votre pseudo"> </td>
+            </tr>
+            <tr>
+                <td>Mot de passe</td>
+                <td> <input type="password" name="password" placeholder="votre mot de passe"> </td>
+            </tr>
+        </table>
+        <button>Connexion</button>
+    </form>
+</body>
+</html>
 
 
